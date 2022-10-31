abf61f41d2037bb2a6b9b5cbf2064ca15acf680a
0kage-eth <0kage.eth@gmail.com>
2022-10-31 14:22:01 +0530

    change imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import * as React from "react"
 import { Box, useColorModeValue } from "@chakra-ui/react"
-import { RouterProvider } from "react-router-dom"
-import { routerConfig } from "./components/router/Router"
+// import { RouterProvider } from "react-router-dom"
+// import { routerConfig } from "./components/router/Router"
 
 const App = () => {
   return (
     <Box bg={useColorModeValue("gray.100", "gray.800")} width="100%">
-      <RouterProvider router={routerConfig} />
+      <p>Hi</p>
+      {/* <RouterProvider router={routerConfig} /> */}
     </Box>
   )
 }
