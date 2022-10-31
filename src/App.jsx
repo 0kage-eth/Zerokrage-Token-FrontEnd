73ae9103ee3c7209e2ee02f4ed05d786e2b5ab4a
0kage-eth <0kage.eth@gmail.com>
2022-10-31 14:28:15 +0530

    add route on root

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import * as React from "react"
 import { Box, useColorModeValue } from "@chakra-ui/react"
-// import { RouterProvider } from "react-router-dom"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 // import { routerConfig } from "./components/router/Router"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+])
 
 const App = () => {
   return (
     <Box bg={useColorModeValue("gray.100", "gray.800")} width="100%">
-      <p>Hi</p>
+      <p>Hi Sup</p>
       {/* <RouterProvider router={routerConfig} /> */}
+      <RouterProvider router={router} />
     </Box>
   )
 }
