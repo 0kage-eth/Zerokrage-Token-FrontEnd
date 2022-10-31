d3367d9272249a1cd63ef38fad2a2d9340448154
0kage-eth <0kage.eth@gmail.com>
2022-10-31 15:01:05 +0530

    change folder name

--- src/Pages/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import { PageHeader } from "../components/headers/PageHeader"
-
-export const Dashboard = () => {
-  return (
-    <div>
-      <PageHeader title="Dashboard" subtitle="Overall token stats" />
-    </div>
-  )
-}
